b8a45c18ca32a8b3d7324b68d4acbab9803a2472
Slobodan Prijic <slobodan@primo.me>
2017-12-11 23:59:08 +0100

    Added basic notification managing singleton

---
+++ toolbox/src/main/java/com/hyperether/toolbox/HyperNotification.java
@@ -0,0 +1,56 @@
+package com.hyperether.toolbox;
+
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.content.Context;
+import android.graphics.BitmapFactory;
+
+/*
+ * HyperNotification.java
+ *
+ * Created by Slobodan on 12/11/2017
+ */
+
+public class HyperNotification {
+
+    private static HyperNotification instance;
+    private int notificationId = 0;
+    private String channelId = "hyper_channel_1";
+
+    public static HyperNotification getInstance() {
+        if (instance == null)
+            instance = new HyperNotification();
+        return instance;
+    }
+
+    public Notification getForegroundServiceNotification(Context context,
+                                                         String title,
+                                                         String status,
+                                                         int iconSmall,
+                                                         int iconLarge) {
+        return new Notification.Builder(context)
+                .setContentTitle(title)
+                .setContentText(status)
+                .setSmallIcon(iconSmall)
+                .setLargeIcon(BitmapFactory.decodeResource(context.getResources(), iconLarge))
+                .build();
+    }
+
+    protected void setNotification(Context context, Notification notification) {
+        NotificationManager mNotificationManager = (NotificationManager) context.getSystemService(
+                Context.NOTIFICATION_SERVICE);
+        if (mNotificationManager != null) {
+            notificationId++;
+            mNotificationManager.notify(notificationId, notification);
+        }
+    }
+
+    protected void removeNotification(Context context) {
+        NotificationManager mNotificationManager = (NotificationManager) context.getSystemService(
+                Context.NOTIFICATION_SERVICE);
+        if (mNotificationManager != null) {
+            mNotificationManager.cancel(notificationId);
+            notificationId--;
+        }
+    }
+}
